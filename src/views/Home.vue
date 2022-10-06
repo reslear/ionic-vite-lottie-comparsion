3c10fce1bbe03a1cf13a0192c782e4917650c5a9
reslear <reslear@gmail.com>
2022-10-06 20:05:54 +0200

    feat: add lottiefiles and dynamicLoadModule player

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,8 +6,33 @@
       </ion-toolbar>
     </ion-header>
     <ion-content class="ion-padding">
-      <h1>Home</h1>
-      <ion-button router-link="/about">About</ion-button>
+      <div>
+        <h2>Dynamic import</h2>
+        <ion-button @click="startDynamicLoadModule()">Start</ion-button>
+        <div v-if="dynamicLoadModule.json">
+          <div>time: {{ dynamicLoadModule.time }}ms</div>
+        </div>
+      </div>
+
+      <div>
+        <h2>Dot Lottie Player</h2>
+        <ion-button @click="startDotLottiePlayer()">Start</ion-button>
+
+        <div v-if="dotLottiePlayer.display">
+          <div>time: {{ dotLottiePlayer.time }}ms</div>
+
+          <lottie-player
+            ref="dotLottiePlayerRef"
+            autoplay
+            controls
+            loop
+            mode="normal"
+            :src="dotLottiePlayer.jsonFileUrl"
+            style="width: 320px"
+            @ready="endDotLottiePlayer()"
+          ></lottie-player>
+        </div>
+      </div>
     </ion-content>
   </ion-page>
 </template>
@@ -21,6 +46,54 @@ import {
   IonTitle,
   IonToolbar,
 } from '@ionic/vue'
+import { reactive, ref, shallowReactive, shallowRef } from 'vue'
+import * as LottiePlayer from '@lottiefiles/lottie-player'
+
+const jsonFile = '../assets/private-events.json'
+
+function getFileUrl() {
+  const url = new URL(jsonFile, import.meta.url).href
+  return `${url}?${+new Date()}`
+}
+
+const dynamicLoadModule = shallowReactive({
+  json: '',
+  time: 0,
+})
+
+const dotLottiePlayer = shallowReactive({
+  startTime: 0,
+  time: 0,
+  display: false,
+  jsonFileUrl: '',
+})
+const dotLottiePlayerRef = ref()
+
+// output with Module Dynamic Load
+// dist/assets/private-events.408775f5.js   711.97 KiB / gzip: 42.89 KiB
+async function startDynamicLoadModule() {
+  const start = window.performance.now()
+
+  await import('../assets/private-events.json').then((module) => {
+    dynamicLoadModule.json = JSON.stringify(module.default)
+  })
+
+  const end = window.performance.now()
+  dynamicLoadModule.time = Math.round(end - start)
+}
+
+async function startDotLottiePlayer() {
+  Object.assign(dotLottiePlayer, {
+    startTime: window.performance.now(),
+    display: true,
+    jsonFileUrl: getFileUrl(),
+  })
+}
+
+async function endDotLottiePlayer() {
+  const end = window.performance.now()
+  dotLottiePlayer.time = Math.round(end - dotLottiePlayer.startTime)
+}
 </script>
 
 <style></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => tag === 'lottie-player',
+        },
+      },
+    }),
+    visualizer({
+      open: true,
+      brotliSize: true,
+      gzipSize: true,
+    }),
+  ],
+  css: {
+    devSourcemap: false,
+  },
 })
